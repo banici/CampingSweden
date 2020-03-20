c4b25243960e22c0a40fd5e3e4d7283db397936a
ille Banic <banicworld@gmail.com>
2020-03-20 14:16:18 +0100

    added models folder with camping park class file

---
+++ campingSweden/CampingParkAPI/Models/CampingPark.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampingParkAPI.Models
+{
+    public class CampingPark
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string State { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Established { get; set; }
+    }
+}
