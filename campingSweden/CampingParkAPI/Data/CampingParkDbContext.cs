5ce37f02dc53c8c3910bc72d775909e5b1c80a97
banicworld <banicworld@gmail.com>
2020-03-21 18:52:24 +0100

    added dbContext

--- campingSweden/CampingParkAPI/Data/CampingParkDbContext.cs
+++ campingSweden/CampingParkAPI/Data/CampingParkDbContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using CampingParkAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CampingParkAPI.Data
 {
-    public class CampingParkDbContext
+    public class CampingParkDbContext : DbContext
     {
+        public CampingParkDbContext(DbContextOptions<CampingParkDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CampingPark> CampingParks { get; set; }
     }
 }
