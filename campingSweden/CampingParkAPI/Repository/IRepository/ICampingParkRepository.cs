1da45ed50f27649e70067b3599fc54211fd7d561
banicworld <banicworld@gmail.com>
2020-03-21 21:45:50 +0100

    some files edited

--- campingSweden/CampingParkAPI/Repository/IRepository/ICampingParkRepository.cs
+++ campingSweden/CampingParkAPI/Repository/IRepository/ICampingParkRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CampingParkAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,10 @@ namespace CampingParkAPI.Repository.IRepository
 {
     public interface ICampingParkRepository
     {
+        CampingPark GetAllCampingParks();
+        CampingPark GetCampingPark(int id);
+        bool DoesParkExist(CampingPark cPark);
+        bool DoesParkExist(int id);
+
     }
 }
