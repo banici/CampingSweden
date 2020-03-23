640bf1616b5277586de80d4bb0af4e0cd2c99a0f
banicworld <banicworld@gmail.com>
2020-03-23 13:07:14 +0100

    minor editing in repo

--- campingSweden/CampingParkAPI/Repository/CampingParkRepository.cs
+++ campingSweden/CampingParkAPI/Repository/CampingParkRepository.cs
@@ -1,11 +1,59 @@
-﻿using System;
+﻿using CampingParkAPI.Data;
+using CampingParkAPI.Models;
+using CampingParkAPI.Repository.IRepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CampingParkAPI.Repository
 {
-    public class CampingParkRepository
+    public class CampingParkRepository : ICampingParkRepository
     {
+        private readonly CampingParkDbContext _context;
+
+        public CampingParkRepository(CampingParkDbContext context)
+        {
+            _context = context;
+        }
+        public bool CampingParkExist(string name)
+        {
+            return _context.CampingParks.Any(c => c.Name.ToLower().Trim() == name.ToLower().Trim());
+        }
+
+        public bool CampingParkExist(int id)
+        {
+            return _context.CampingParks.Any(c => c.Id == id);
+        }
+
+        public bool CreateCampingPark(CampingPark cPark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteCampingPark(CampingPark cPark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<CampingPark> GetAllCampingParks()
+        {
+            return _context.CampingParks.OrderBy(c => c.Name).ToList();
+        }
+
+        public CampingPark GetCampingPark(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            return _context.SaveChanges() >= 0 ? true : false;
+        }
+
+        public bool UpdateCampingPark(CampingPark cPark)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
