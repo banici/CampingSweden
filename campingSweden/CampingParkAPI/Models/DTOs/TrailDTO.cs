72386f1f86b9c7dd4de04236f4fcab5a3de82859
banicworld <banicworld@gmail.com>
2020-03-25 02:01:39 +0100

    added new classmodel and DTO Trail

---
+++ campingSweden/CampingParkAPI/Models/DTOs/TrailDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using static CampingParkAPI.Models.Trail;
+
+namespace CampingParkAPI.Models.DTOs
+{
+    public class TrailDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Distance { get; set; }
+
+        public DifficultyType Difficulty { get; set; }
+
+        [Required]
+        public int CampingParkId { get; set; }
+
+        public CampingParkDTO CampingPark { get; set; }
+    }
+}
---
+++ campingSweden/CampingParkAPI/Models/Trail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampingParkAPI.Models
+{
+    public class Trail
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Distance { get; set; }
+
+        public enum DifficultyType { Easy, Medium, Hard, Expert }
+
+        public DifficultyType Difficulty { get; set; }
+
+        [Required]
+        public int CampingParkId { get; set; }
+
+        [ForeignKey("CampingParkId")]
+        public CampingPark CampingPark { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
