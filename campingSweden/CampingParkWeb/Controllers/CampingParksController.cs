dff6d7acb7b83765a94a27ba34b9c9a94b21debc
banicworld <banicworld@gmail.com>
2020-03-26 14:34:39 +0100

    added cPark controller in MVC

---
+++ campingSweden/CampingParkWeb/Controllers/CampingParksController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CampingParkWeb.Models;
+using CampingParkWeb.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CampingParkWeb.Controllers
+{
+    public class CampingParksController : Controller
+    {
+        private readonly ICampingParkRepository _cpRepo;
+
+        public CampingParksController(ICampingParkRepository cpRepo)
+        {
+            _cpRepo = cpRepo;
+        }
+        public IActionResult Index()
+        {
+            return View(new CampingPark { });
+        }
+
+        public async Task<IActionResult> GetAllCampingParks()
+        {
+            return Json(new { data = _cpRepo.GetAllAsync(StaticDetails.CampingParkAPIPath) }); 
+        }
+    }
+}
