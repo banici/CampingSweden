56013c147f03cc7db2f345647d4b2820138b12f9
banicworld <banicworld@gmail.com>
2020-03-26 17:57:58 +0100

    Upsert method complete & Upsert view created

--- campingSweden/CampingParkWeb/Controllers/CampingParksController.cs
+++ campingSweden/CampingParkWeb/Controllers/CampingParksController.cs
@@ -21,6 +21,27 @@ namespace CampingParkWeb.Controllers
             return View(new CampingPark() { });
         }
 
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            CampingPark obj = new CampingPark();
+
+            if(id == null)
+            {
+                // this will be true for insert/create
+                return View(obj);
+            }
+
+            // this will come here for update.
+            obj = await _cpRepo.GetAsync(StaticDetails.CampingParkAPIPath, id.GetValueOrDefault());
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
         public async Task<IActionResult> GetAllCampingPark()
         {
             return Json(new { data = await _cpRepo.GetAllAsync(StaticDetails.CampingParkAPIPath) }); 
