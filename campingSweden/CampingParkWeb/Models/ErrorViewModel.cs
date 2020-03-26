379b94d836bdd938dfd15035f584d53dee3c33cf
banicworld <banicworld@gmail.com>
2020-03-26 05:16:53 +0100

    added new project MVC CampingParkWeb

---
+++ campingSweden/CampingParkWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CampingParkWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
