b530754a2a55b0b976eb7349067653ac61f52383
banicworld <banicworld@gmail.com>
2020-03-26 05:26:14 +0100

    added mvc class static details with API Url paths

---
+++ campingSweden/CampingParkWeb/StaticDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampingParkWeb
+{
+    public static class StaticDetails
+    {
+        public static string APIBaseUrl = "https://localhost:44360/";
+
+        public static string CampingParkAPIPath = APIBaseUrl + "api/v1/campingparks";
+        public static string TrailAPIPath = APIBaseUrl + "api/v1/trails";
+    }
+}
