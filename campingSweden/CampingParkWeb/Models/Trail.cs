a5527553fd8a04b26f67aa917c812d3f570e1a6c
banicworld <banicworld@gmail.com>
2020-03-26 06:25:27 +0100

    added classes in MVC campingpark

---
+++ campingSweden/CampingParkWeb/Models/Trail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampingParkWeb.Models
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Distance { get; set; }
+
+        public enum DifficultyType { Easy, Medium, Hard, Expert }
+
+        public DifficultyType Difficulty { get; set; }
+
+        [Required]
+        public int CampingParkId { get; set; }
+
+        public CampingPark CampingPark { get; set; }
+    }
+}
